6431babd8b9c2bccb03f3a2c5c2dbf9cb72d77f9
sehdevkumar <kumarsehdev600@gmail.com>
2024-09-18 21:27:15 +0530

    form Array

--- src/forms/forms.component.ts
+++ src/forms/forms.component.ts
@@ -1,37 +1,61 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit, signal, effect } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule,ReactiveFormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
 })
 export class FormsComponent implements OnInit {
-  constructor() {}
-
+  fb: FormBuilder = inject(FormBuilder);
   sampleReactiveForm!: FormGroup;
+  addressArray = signal<FormArray<any>>(this.fb.array([]));
 
   ngOnInit() {
+    this.sampleReactiveForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      mail: new FormControl('', [Validators.required, Validators.email]),
+      address: this.addressArray(),
+    });
 
-    this.sampleReactiveForm = new FormGroup<any>({
-
-        name: new FormControl('',Validators.required),
-        mail: new FormControl('',Validators.required)
-
-   } )
-
-
-   this.sampleReactiveForm.setValue({
-     name: 'hey',
-     mail: 'i am here'
-   })
-
+    // Watch for form array changes and update the signal accordingly
+    effect(() => {
+      this.addressArray.set(
+        this.sampleReactiveForm.get('address') as FormArray
+      );
+    });
   }
 
-  onFormSubmit({target}: any) {
-     console.log(target.name.value,target.mail.value,this.sampleReactiveForm.invalid);
+  addAddress() {
+    const city = this.fb.control('', Validators.required);
+    const zipCode = this.fb.control('', Validators.required);
+    this.addressArray().push(
+      this.fb.group({
+        city: city,
+        zipCode: zipCode,
+      })
+    );
+  }
+
+  onFormSubmit(event?: any) {
+    if (this.sampleReactiveForm.valid) {
+      console.log(this.sampleReactiveForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+
+      console.log(this.sampleReactiveForm.value);
+
   }
 }
